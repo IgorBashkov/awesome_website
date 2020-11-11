cfd8843a8ae3e03c2bfcf19d8877668e69cb647f
IgorBashkov <bashkov.torvm36@gmail.com>
2020-11-11 22:25:23 +0300

    Start project from RealPython

--- awesome_website/settings.py
+++ awesome_website/settings.py
@@ -26,19 +26,19 @@ SECRET_KEY = KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
 ]
 
 MIDDLEWARE = [
